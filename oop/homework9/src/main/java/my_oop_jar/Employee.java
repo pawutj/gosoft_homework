e1ead7b07e8ed3a63c70a278483ab6162355c329
22p26c0377 <pawut.j@gmail.com>
2021-11-10 03:55:00 +0800

    Add gossip function

--- oop/homework9/src/main/java/my_oop_jar/Employee.java
+++ oop/homework9/src/main/java/my_oop_jar/Employee.java
@@ -1,19 +1,26 @@
 package my_oop_jar;
 
 public class Employee {
-    public   String firstname;
-    public   String lastname;
-    private  int salary;
-   
+    public String firstname;
+    public String lastname;
+    private int salary;
+
     public Employee(String firstnameInput, String lastnameInput, int salaryInput) {
         this.firstname = firstnameInput;
         this.lastname = lastnameInput;
         this.salary = salaryInput;
     }
+
     public void hello() {
-        System.out.println("Hello " + this.firstname );
+        System.out.println("Hello " + this.firstname);
     }
+
     public int getSalary() {
         return salary;
+    }
+
+    // Add function gossip
+    public void gossip(Employee c, String s) {
+        return;
     }
 }
