bb683900d7fb0c8211b4fa78ebfada3790e2d23e
22p26c0377 <pawut.j@gmail.com>
2021-11-12 23:33:20 +0700

    test ArrayList

--- collection/Collect_hw1.java
+++ collection/Collect_hw1.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 
 class Collect_hw1 {
     public static void main(String[] args) {
-        String[] rawData = { "id:1001 firstname:Luke lastname:Skywalker" };
+        String[] rawData = { "id:1001 firstname:Luke lastname:Skywalker", "id:1002 firstname:Luke lastname:Skywalker" };
+        ArrayList<String> data = new ArrayList<>(Arrays.asList(rawData));
+        System.out.println(data);
     }
 
 }
