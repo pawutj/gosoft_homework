5c721e982cfa65da13e04add8b3f161d30f64775
22p26c0377 <pawut.j@gmail.com>
2021-11-13 00:25:23 +0800

    Add Collection Homework

---
+++ collection/Collect_hw1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+class Collect_hw1 {
+    public static void main(String[] args) {
+        String[] rawData = { "id:1001 firstname:Luke lastname:Skywalker" };
+    }
+
+}
