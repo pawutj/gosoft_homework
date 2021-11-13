dad1ca76920330dc1fa8378d93193ca684e1176a
22p26c0377 <pawut.j@gmail.com>
2021-11-14 00:12:06 +0800

    Project Complate

--- project_ecom/Project.java
+++ project_ecom/Project.java
@@ -33,15 +33,49 @@ public class Project {
         int i = input.nextInt();
         System.out.println(i);
         String s = String.valueOf(i);
-        Product p = productList.getProduct("123");
+        Product p = productList.getProduct(s);
         state.addProductToCart(p);
+
+        System.out.println("done");
+    }
+
+    public static void CustomerRemoveProductFromCart(State state, ProductList productList) {
+        System.out.println("What Product id which you remove?");
+
+        int i = input.nextInt();
+        System.out.println(i);
+        String s = String.valueOf(i);
+        state.removeProductFromCart(s);
+
+        System.out.println("done");
+    }
+
+    public static void SellerAddProduct(State state, ProductList productList) {
+        System.out.println("What Product ID which you want to add?");
+        String s1 = input.next();
+        System.out.println("Name?");
+        String s2 = input.next();
+        System.out.println("Price?");
+        int s3 = input.nextInt();
+
+        Product p = new Product(s1, s2, s3);
+
+        productList.addProduct(p);
+
+        System.out.println("done");
+    }
+
+    public static void SellerRemoveProduct(State state, ProductList productList) {
+        System.out.println("What Product ID which you want to Remove?");
+        String s1 = input.next();
+        productList.removeProduct(s1);
 
         System.out.println("done");
     }
 
     public static void readCustomerCommand(State state, ProductList productList) {
         System.out.println(
-                "What do you do?\n 0.Show All Product in Shop\n 1.Show All Product In Cart\n 2.Add Product to Cart\n 3.Remove Product From Cart\n");
+                "What do you do?\n 0.Show All Product in Shop\n 1.Show All Product In Cart\n 2.Add Product to Cart\n 3.Remove Product From Cart\n 4.Back to main");
 
         int n = input.nextInt();
         if (n == 0)
@@ -51,18 +85,30 @@ public class Project {
         if (n == 2) {
             CustomerAddProductToCart(state, productList);
         }
-        // if (n == 3) {
-        // String s = input.nextLine();
-        // state.removeProductFromCart(s);
-        // }
+        if (n == 3) {
+            CustomerRemoveProductFromCart(state, productList);
+        }
+
+        if (n == 4) {
+            state.isMain = false;
+        }
 
         readCommand(state, productList);
 
     }
 
     public static void readAdminCommand(State state, ProductList productList) {
-        System.out.println("What do you do?\n 0.Show All Product\n 1.Add Product\n 2.Remove Product\n");
-        // int n = input.nextInt();
+        System.out.println("What do you do?\n 0.Show All Product\n 1.Add Product\n 2.Remove Product\n 3.Back to main");
+        int n = input.nextInt();
+        if (n == 0)
+            productList.showAllProduct();
+        if (n == 1)
+            SellerAddProduct(state, productList);
+        if (n == 2)
+            SellerRemoveProduct(state, productList);
+        if (n == 3)
+            state.isMain = true;
+        readCommand(state, productList);
     }
 
     public static void main(String[] args) {
