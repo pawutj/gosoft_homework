05ec1fca06452a5d2a474d30767ecbb9053ef1db
22p26c0377 <pawut.j@gmail.com>
2021-11-17 17:46:34 +0800

    Add lab1 Spring

---
+++ lab1spring/src/main/java/lab1spring/lab1spring/Lab1springApplication.java
@@ -0,0 +1,13 @@
+package lab1spring.lab1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1springApplication.class, args);
+	}
+
+}
---
+++ lab1spring/src/test/java/lab1spring/lab1spring/Lab1springApplicationTests.java
@@ -0,0 +1,13 @@
+package lab1spring.lab1spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
