0e28b52285c801801d96740c1b6f216bda2cb7ee
22p26c0377 <pawut.j@gmail.com>
2021-11-19 04:56:32 +0800

    Add SpringHomework1 to repo

---
+++ springboothomework1/src/main/java/com/springboothomework1/springboothomework1/Springboothomework1Application.java
@@ -0,0 +1,13 @@
+package com.springboothomework1.springboothomework1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboothomework1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboothomework1Application.class, args);
+	}
+
+}
---
+++ springboothomework1/src/test/java/com/springboothomework1/springboothomework1/Springboothomework1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboothomework1.springboothomework1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboothomework1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
