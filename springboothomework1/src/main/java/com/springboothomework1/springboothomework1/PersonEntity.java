f574eccc1d977f5560642d6ed9f980759b8cbefc
22p26c0377 <pawut.j@gmail.com>
2021-11-20 01:57:26 +0800

    Add PersonEntity

---
+++ springboothomework1/src/main/java/com/springboothomework1/springboothomework1/PersonEntity.java
@@ -0,0 +1,18 @@
+package com.springboothomework1.springboothomework1;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Data;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Data
+@Entity
+public class PersonEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+    private String fullName;
+    private Integer status;
+}
