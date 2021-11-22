713c6e6a70a080f2a41e51fbb5990d1d79355cea
22p26c0377 <pawut.j@gmail.com>
2021-11-22 11:58:52 +0800

    ADD Controller

---
+++ springboothomework1/src/main/java/com/springboothomework1/springboothomework1/PersonController.java
@@ -0,0 +1,29 @@
+package com.springboothomework1.springboothomework1;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+    @Autowired
+    PersonService personService;
+
+    @GetMapping()
+    public List<Person> getPersons() {
+        return personService.retrievePerson();
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> postPerson(@RequestBody Person body) {
+        Person person = personService.createPerson(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(person);
+    }
+}
--- springboothomework1/src/main/java/com/springboothomework1/springboothomework1/PersonService.java
+++ springboothomework1/src/main/java/com/springboothomework1/springboothomework1/PersonService.java
@@ -3,22 +3,28 @@ package com.springboothomework1.springboothomework1;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonService {
+    @Autowired
     private PersonRepository personRepository;
 
-    @Autowired
-    public PersonService(PersonRepository repository) {
-        this.personRepository = repository;
-    }
+    // public PersonService(PersonRepository repository) {
+    // this.personRepository = repository;
+    // }
 
     public List<Person> retrievePerson() {
         return (List<Person>) personRepository.findAll();
     }
 
+    public Optional<Person> retrievePerson(Long id) {
+        return personRepository.findById(id);
+
+    }
+
     public Person createPerson(Person person) {
-        person.setId(null);
+        // person.setId(1);
         return personRepository.save(person);
 
     }
