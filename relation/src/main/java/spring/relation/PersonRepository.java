14a4b81ef9be94779da580261f84dbf7bd1dbc1e
22p26c0377 <pawut.j@gmail.com>
2021-11-24 01:33:04 +0800

    add person repositorytest

--- relation/src/main/java/spring/relation/PersonRepository.java
+++ relation/src/main/java/spring/relation/PersonRepository.java
@@ -2,8 +2,9 @@ package spring.relation;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Long> {
-
+    Optional<Person> findByFullName(String fullname);
 }
---
+++ relation/src/test/java/spring/relation/PersonRepositoryTest.java
@@ -0,0 +1,38 @@
+package spring.relation;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class PersonRepositoryTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        personRepository.deleteAll();
+    }
+
+    @Test
+    public void testFindByFullName() throws Exception {
+        Person person = new Person();
+        person.setFullName("astralair");
+        entityManager.persist(person);
+
+        Optional<Person> personOptional = personRepository.findByFullName("astralair");
+
+        assertThat(personOptional.get().getFullName()).isEqualTo("astralair");
+
+    }
+}
