bb44242017723cc1bea64388114b6898c576be5d
22p26c0377 <pawut.j@gmail.com>
2021-11-24 18:42:48 +0800

    Add Ecom Project Fix

---
+++ project_ecom_fix/ecom/src/main/java/project_ecom_fix/App.java
@@ -0,0 +1,13 @@
+package project_ecom_fix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
