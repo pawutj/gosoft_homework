ec442f4336204beee44b393bd9aa6b8bf33ac2e9
22p26c0377 <pawut.j@gmail.com>
2021-11-30 23:10:59 +0800

    Fix bug

---
+++ authen/login/src/main/java/authen/login/Controller/ViewPageController.java
@@ -0,0 +1,14 @@
+package authen.login.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/view")
+public class ViewPageController {
+    @GetMapping("/test")
+    public String viewtest() {
+        return "HELLO WORLD";
+    }
+}
