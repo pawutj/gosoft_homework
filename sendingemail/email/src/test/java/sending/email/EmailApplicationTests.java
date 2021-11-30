97c3b5cc1716ac6270f76323f5ef216543971ab3
22p26c0377 <pawut.j@gmail.com>
2021-11-30 16:40:17 +0800

    Init Email Sending

---
+++ sendingemail/email/src/test/java/sending/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package sending.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
