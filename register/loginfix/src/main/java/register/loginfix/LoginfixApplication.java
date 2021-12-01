d6b9bd3931c19572406aeb71219e08ea7b1e17c9
22p26c0377 <pawut.j@gmail.com>
2021-12-01 14:22:32 +0800

    Fix configuration

---
+++ register/loginfix/src/main/java/register/loginfix/LoginfixApplication.java
@@ -0,0 +1,13 @@
+package register.loginfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginfixApplication.class, args);
+	}
+
+}
